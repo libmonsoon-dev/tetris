6b1472a9dcc0899b7e009267c5a370d55578d7af
user <daniil.stepanenko.96@gmail.com>
2019-11-17 13:41:30 +0200

    add some game.Shape methods

--- pkg/game/shape.go
+++ pkg/game/shape.go
@@ -5,4 +5,14 @@ import "tetris/pkg/shape"
 type Shape struct {
 	shape.Shape
 	Index int
+	Color
+}
+
+func (sh Shape) GetCurrentState() shape.State {
+	return sh.Shape[sh.Index]
+}
+
+func (sh *Shape) NextState() {
+	sh.Index++
+	sh.Index %= len(sh.Shape)
 }
